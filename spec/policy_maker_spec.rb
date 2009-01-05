281041b423db73db1b9ffffae106fd9f8ca90f6e
Fred Medlin <fmedlin@gmail.com>
2009-01-05 17:32:43 -0500

    Some test cleanup

--- spec/policy_maker_spec.rb
+++ spec/policy_maker_spec.rb
@@ -22,41 +22,36 @@ describe PolicyMaker do
   end  
   
   it "should create encrypt, drop and clear policies" do
-    encrypt_policy = @policy_defs.select { |p| p.encrypt? }
-    encrypt_policy.size.should equal(1)
-
-    drop_policy = @policy_defs.select { |p| p.drop? }
-    drop_policy.size.should equal(1)
-
-    pass_policy = @policy_defs.select { |p| p.pass? }
-    pass_policy.size.should equal(2)
+    @policy_defs.select { |p| p.encrypt? }.size.should equal(1)
+    @policy_defs.select { |p| p.drop? }.size.should equal(1)
+    @policy_defs.select { |p| p.pass? }.size.should equal(2)
   end
   
-  it "should create server pep encryption policies" do
-    pep_policy = @policy_defs.first
-    pep_policy.topology.should == 'mesh'
-    pep_policy.encrypt_alg.should == 'aes'
-    pep_policy.auth_alg.should == 'sha1'
+  it "should create the correct encrypt options" do
+    policy = @policy_defs.first
+    policy.topology.should == 'mesh'
+    policy.encrypt_alg.should == 'aes'
+    policy.auth_alg.should == 'sha1'
   end
   
-  it "should only create policies for server peps" do
+  it "should only generate policies for server peps" do
     @pmaker.policies.size.should equal(@servers.size)
     @pmaker.policy_for('unknown_server').should be_nil
   end
   
-  it "should generate encrypt policies for each server pep" do
+  it "should generate encrypt policies for server peps" do
     @sinatra_policy.instance_of?(PepPolicy).should be_true
     @sinatra_policy.encrypt_rules.size.should equal(4)
     @sinatra_policy.rules.first.src_ip.should == IPAddr.new('192.168.1.1')
     @sinatra_policy.rules.first.dst_ip.should == IPAddr.new('192.168.1.3')
   end
   
-  it "should generate drop policies for each server pep only" do
+  it "should generate drop policies for server peps" do
     @sinatra_policy.drop_rules.size.should equal(4)
     @elvis_policy.drop_rules.size.should equal(4)
   end
   
-  it "should generate clear policies for each server pep only" do
+  it "should generate clear policies for server peps" do
     @sinatra_policy.pass_rules.size.should equal(0)
   end
     
@@ -89,13 +84,14 @@ describe PolicyMaker do
   # policy definitions
   describe "policy definitions" do
     
+    it "should be created" do
+      @policy_defs.size.should equal(4)
+    end
+
     it "should detect server peps" do
       @policy_defs.first.includes_server?(ServerPep.new('dont-care', '192.168.1.1')).should be_true
     end
     
-    it "should add policy definitions" do
-      @policy_defs.size.should equal(4)
-    end
   end
   
   # solaris ipsecconf
